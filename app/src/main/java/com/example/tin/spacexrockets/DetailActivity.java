9e88020e43a8322a84e583451d36739cfd551555
Tin Novakovic <tinnovamail@gmail.com>
2018-08-16 09:20:25 +0100

    feat: RocketLaunch RecyclerView displaying data

--- app/src/main/java/com/example/tin/spacexrockets/DetailActivity.java
+++ app/src/main/java/com/example/tin/spacexrockets/DetailActivity.java
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,8 @@ public class DetailActivity extends AppCompatActivity {
 
     private static final String TAG = DetailActivity.class.getSimpleName();
 
+    private RocketLaunchAdapter mAdapter;
+    private RecyclerView mRecyclerView;
     private DetailViewModel detailViewModel;
 
     private TextView descTv;
@@ -39,16 +43,27 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
 
-        if (getIntent != null) {
+        if (getIntent == null) {
+
+            Toast.makeText(this, "Error loading data, please try again.", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            /* Setting up the RecyclerView and Adapter*/
+            mRecyclerView = findViewById(R.id.rV_detail);
+            mRecyclerView.setHasFixedSize(true);
+            LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+            mAdapter = new RocketLaunchAdapter(getApplicationContext());
+            mRecyclerView.setAdapter(mAdapter);
 
             String rocketId = getIntent.getStringExtra("ID");
             descTv.setText(getIntent.getStringExtra("DESC"));
 
             bindOnViewModel(rocketId);
-
-        } else {
-            Toast.makeText(this, "Error loading data, please try again.", Toast.LENGTH_SHORT).show();
         }
+
     }
 
     private void bindOnViewModel(String rocketId) {
@@ -59,7 +74,7 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onChanged(@android.support.annotation.Nullable ArrayList<RocketLaunchResponse> response) {
 
-                //mAdapter.addItems(rocketResponse);
+                mAdapter.addItems(response);
 
 //                Log.i(TAG, "response MissionName: " + response.get(0).getMissionName());
 //                Log.i(TAG, "response Rocket Id: " + response.get(0).getRocket().getRocketId());
---
+++ app/src/main/java/com/example/tin/spacexrockets/RocketLaunchAdapter.java
@@ -0,0 +1,89 @@
+package com.example.tin.spacexrockets;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tin.spacexrockets.models.rocketLaunch.RocketLaunchResponse;
+
+
+import java.util.ArrayList;
+
+
+public class RocketLaunchAdapter extends RecyclerView.Adapter<RocketLaunchAdapter.ViewHolder> {
+
+    private ArrayList<RocketLaunchResponse> mRocketLaunchResponse;
+    private Context mContext;
+    private LayoutInflater layoutInflater;
+
+    public RocketLaunchAdapter(Context mContext) {
+
+        mContext = mContext;
+        this.mRocketLaunchResponse = new ArrayList<>();
+        this.layoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public RocketLaunchAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        View v = layoutInflater.inflate(R.layout.rocket_launch_list_item, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RocketLaunchAdapter.ViewHolder viewHolder, int position) {
+
+        RocketLaunchResponse rocketLaunch = mRocketLaunchResponse.get(position);
+
+        viewHolder.launchYear.setText(rocketLaunch.getLaunchYear());
+        viewHolder.missionNameTv.setText(rocketLaunch.getMissionName());
+        viewHolder.launchDateTv.setText(rocketLaunch.getLaunchDateUtc());
+        viewHolder.launchSuccessTv.setText(Boolean.toString(rocketLaunch.getLaunchSuccess()));
+
+
+        /** Picasso required to insert image of Mission_Patch */
+//        viewHolder.launchSuccessTv.setText(rocketLaunch.getLinks().getMissionPatchSmall());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRocketLaunchResponse == null) {
+            return 0;
+        } else {
+            return mRocketLaunchResponse.size();
+        }
+    }
+
+    public void addItems(ArrayList<RocketLaunchResponse> listToAdd) {
+
+        this.mRocketLaunchResponse.clear();
+        this.mRocketLaunchResponse.addAll(listToAdd);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        final TextView launchYear;
+        final TextView missionNameTv;
+        final TextView launchDateTv;
+        final TextView launchSuccessTv;
+        //final ImageView missionPatchIv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            launchYear = itemView.findViewById(R.id.tV_launchYear);
+            missionNameTv = itemView.findViewById(R.id.tV_missionName);
+            launchDateTv = itemView.findViewById(R.id.tV_launchDate);
+            launchSuccessTv = itemView.findViewById(R.id.tV_launchSuccess);
+//            missionPatchIv = itemView.findViewById(R.id.iV_missionPatch);
+
+        }
+    }
+}
